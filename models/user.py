f609beb789510c5fc1b45ecf27b92e7a9a8fa4c9
Kevin Koech <kevinkoech@proton.me>
2023-09-21 01:22:24 +0300

    add modelling schema for user

--- models/user.py
+++ models/user.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+
+"""
+This module defines a class User
+"""
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+
+class User(BaseModel, Base):
+    """
+    This class defines a user by various attributes
+    """
+    # email = ''
+    # password = ''
+    # first_name = ''
+    # last_name = ''
+
+    __tablename__ = 'users'
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
