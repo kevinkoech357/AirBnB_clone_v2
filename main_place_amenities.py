f4a08dcb95a84a6fb7bed54806ab0dd7658bd0f0
riikakelvin <riikakelvin2@gmail.com>
2023-09-20 12:52:35 -0700

    commit for addition of test_script directory containing testing modules

---
+++ main_place_amenities.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" Test link Many-To-Many Place <> Amenity
+"""
+from models import *
+
+# creation of a State
+state = State(name="California")
+state.save()
+
+# creation of a City
+city = City(state_id=state.id, name="San Francisco")
+city.save()
+
+# creation of a User
+user = User(email="john@snow.com", password="johnpwd")
+user.save()
+
+# creation of 2 Places
+place_1 = Place(user_id=user.id, city_id=city.id, name="House 1")
+place_1.save()
+place_2 = Place(user_id=user.id, city_id=city.id, name="House 2")
+place_2.save()
+
+# creation of 3 various Amenity
+amenity_1 = Amenity(name="Wifi")
+amenity_1.save()
+amenity_2 = Amenity(name="Cable")
+amenity_2.save()
+amenity_3 = Amenity(name="Oven")
+amenity_3.save()
+
+# link place_1 with 2 amenities
+place_1.amenities.append(amenity_1)
+place_1.amenities.append(amenity_2)
+
+# link place_2 with 3 amenities
+place_2.amenities.append(amenity_1)
+place_2.amenities.append(amenity_2)
+place_2.amenities.append(amenity_3)
+
+storage.save()
+
+print("OK")
+
+guillaume@ubuntu:~/AirBnB_v2$ 
+guillaume@ubuntu:~/AirBnB_v2$ HBNB_MYSQL_USER=hbnb_dev HBNB_MYSQL_PWD=hbnb_dev_pwd HBNB_MYSQL_HOST=localhost HBNB_MYSQL_DB=hbnb_dev_db HBNB_TYPE_STORAGE=db ./main_place_amenities.py
+OK
+guillaume@ubuntu:~/AirBnB_v2$ 
+guillaume@ubuntu:~/AirBnB_v2$ echo 'SELECT * FROM amenities\G' | mysql -uhbnb_dev -p hbnb_dev_db
+Enter password: 
+*************************** 1. row ***************************
+        id: 47321eb8-152a-46df-969a-440aa67a6d59
+created_at: 2017-11-10 04:22:02
+updated_at: 2017-11-10 04:22:02
+      name: Cable
+*************************** 2. row ***************************
+        id: 4a307e7f-68f9-438f-81c0-8325898dda2a
+created_at: 2017-11-10 04:22:02
+updated_at: 2017-11-10 04:22:02
+      name: Oven
+*************************** 3. row ***************************
+        id: b80aec52-d0c9-420a-8471-3254572954b6
+created_at: 2017-11-10 04:22:02
+updated_at: 2017-11-10 04:22:02
+      name: Wifi
