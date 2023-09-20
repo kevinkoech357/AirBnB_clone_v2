b15ce27649b63aa318fa767e7d981188a36b09c4
Kevin Koech <kevinkoech@proton.me>
2023-09-21 00:33:00 +0300

    fix sqlalchemy typo

--- models/state.py
+++ models/state.py
@@ -5,7 +5,7 @@ State Module for HBNB project
 """
 
 from models.base_model import BaseModel, Base
-from sqlaclhemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 import os
 
