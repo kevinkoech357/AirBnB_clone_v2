4319febeb9eb26aace8b4e9825772bd25e767834
Kevin_Koech <kevinkoech@proton.me>
2023-10-25 14:08:11 +0300

    start a simple flask web app that returns Hello HBNB

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This file starts a simple
+Flask web app
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def greet_hbnb():
+    """
+    This function returns
+    Hello HBNB when called.
+    """
+    return "Hello HBNB"
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
