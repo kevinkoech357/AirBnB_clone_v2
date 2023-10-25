38f62af396638c322c5c479d35367b5fecd2d7b4
Kevin_Koech <kevinkoech@proton.me>
2023-10-25 14:18:05 +0300

    add a new route that accepts string arg

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+This file starts a simple
+Flask web app
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    This function returns
+    Hello HBNB when called.
+    """
+    return "Hello HBNB"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    This function returns
+    HBNB when called.
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def show_text(text):
+    """
+    This function returns
+    HBNB when called.
+    """
+    new_text = text.replace('_', ' ')
+    return "C " + new_text
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host='0.0.0.0')
